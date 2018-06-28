1a4e324ba85a4c1cf0e0ecf901c8df4dad9b58a3
Sébastien Fellenberg <sebastien.fellenberg@gmail.com>
2018-06-29 01:38:30 +0200

    enfait la cst le dernier, ajout de la géo mais faut encore la suivre sur les pages

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ require_once 'src/require.php';
 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <link rel="stylesheet" href="public/css/geobik.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 </head>
 <body>
 
@@ -30,3 +31,4 @@ if($db==NULL){
 
 </body>
 </html>
+
--- src/Views/accueil.php
+++ src/Views/accueil.php
@@ -1,6 +1,5 @@
 <?php
 function accueil(){
-
     if(isset($_GET['deconnexion'])){
         $_SESSION=array();
         session_destroy();
@@ -20,9 +19,22 @@ function accueil(){
 ?>
     <div class="container">
         <div class="accueil">
-            <p>Bonjour Visiteur de France, Paris.</p>
+            <h1>Geobik - une nouvelle révolution</h1>
+            <hr/>
+            <p>Ardeo, mihi credite, Patres conscripti (id quod vosmet de me existimatis et facitis ipsi) incredibili quodam amore patriae, qui me amor et subvenire olim impendentibus periculis maximis cum dimicatione capitis, et rursum, cum omnia tela undique esse intenta in patriam viderem, subire coegit atque excipere unum pro universis. Hic me meus in rem publicam animus pristinus ac perennis cum C. Caesare reducit, reconciliat, restituit in gratiam.</p>
+            <hr/>
+            <article>
+                <h3>Hanc regionem diebus</h3>    
+                <ul>
+                    <li>Sed tamen haec cum ita tutius observentur, quidam</li>
+                    <li>Post quorum necem nihilo lenius Gallus</li>
+                    <li>Sed cautela nimia in peiores haeserat plagas, ut</li>
+                    <li>Hac ita persuasione reducti intra moenia</li>
+                </ul>
+            </article>
+             
             <a href="?page=inscription" class="btn btn-dark" role="button">S'inscrire</a>
-            <a href="?page=connexion" class="btn btn-dark" role="button">Se connecter</a>
+            <!-- <a href="?page=connexion" class="btn btn-dark" role="button">Se connecter</a> -->
         </div>
     </div>
 <?php
